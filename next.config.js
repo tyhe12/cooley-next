4b19ebb10ae2bfc44efda677354de6ed054abd45
Tianyi He <htycm1@gmail.com>
2021-05-12 21:26:30 -0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  /* config options here */
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 60 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 5,
+  },
+}
