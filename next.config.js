ace5bcc89083ffa8480764e4f9d63c640a5e7b30
Tianyi He <htycm1@gmail.com>
2021-06-15 22:57:51 -0700

    create included page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+/* eslint-disable */
+const withImages = require('next-images')
+
+module.exports = withImages({
   /* config options here */
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
@@ -6,4 +9,4 @@ module.exports = {
     // number of pages that should be kept simultaneously without being disposed
     pagesBufferLength: 5,
   },
-}
+})
